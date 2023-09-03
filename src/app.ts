b0f12b27c699e9c7c7b5bdffcd004533d7e97815
Seaborn Lee <seabornlee.cn@gmail.com>
2023-09-03 16:21:05 +0800

    Introduce Taro-ui

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react'
 import { useLaunch } from '@tarojs/taro'
 import './app.scss'
+import 'taro-ui/dist/style/index.scss' // 全局引入一次即可
 
 function App({ children }: PropsWithChildren<any>) {
 
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,11 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import Taro, { Component, Config } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import './index.scss'
-
 export default function Index() {
-
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
-
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+        <AtButton type='primary'>按钮文案</AtButton>
     </View>
   )
 }
