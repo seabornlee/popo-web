c24912e6ba1dad3fd979de4f6b8b9a0d9f7fdbde
Seaborn Lee <seabornlee.cn@gmail.com>
2023-09-03 18:16:30 +0800

    Show map with current location

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,20 +1,59 @@
-import Taro, { Component, Config } from '@tarojs/taro'
+import Taro from '@tarojs/taro'
+import { Component } from 'react'
 import { View, Map } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
 import { AtTabBar } from 'taro-ui'
+import 'taro-ui/dist/style/components/tab-bar.scss';
+import 'taro-ui/dist/style/components/icon.scss';
 import './index.scss'
-export default function Index() {
-  return (
-    <View className='index'>
-      <Map />
-      <AtTabBar
-        fixed
-        tabList={[
-          { title: '小组', iconType: 'list'},
-          { title: '地图', iconType: 'map-pin'},
-          { title: '金库', iconType: 'money'}
-        ]}
-      />
-    </View>
-  )
+
+export default class Index extends Component {
+  config = {
+    navigationBarTitleText: 'Home'
+  }
+
+  state = {
+    latitude: null,
+    longitude: null,
+    speed: null,
+    accuracy: null,
+    currentTab: 0,
+  }
+
+  componentDidMount () {
+    Taro.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        this.setState({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          speed: res.speed,
+          accuracy: res.accuracy,
+        })
+      }
+    });
+  }
+
+  handleTabClick = (value) => {
+    this.setState({
+      currentTab: value
+    });
+  }
+
+  render () {
+    return (
+      <View className='index'>
+        <Map longitude={this.state.longitude} latitude={this.state.latitude}/>
+        <AtTabBar
+          fixed
+          current={this.state.currentTab}
+          onClick={this.handleTabClick}
+          tabList={[
+            { title: '小组', iconType: 'list'},
+            { title: '地图', iconType: 'map-pin'},
+            { title: '金库', iconType: 'money'}
+          ]}
+        />
+      </View>
+    )
+  }
 }
