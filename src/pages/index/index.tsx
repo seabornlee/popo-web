113a4f24e7df8d9bd19a0a83b40f6f5e204d4935
Seaborn Lee <seabornlee.cn@gmail.com>
2023-09-04 17:17:54 +0800

    Add marker

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,10 @@
-import Taro from '@tarojs/taro'
-import { Component } from 'react'
-import { View, Map } from '@tarojs/components'
-import { AtTabBar } from 'taro-ui'
+import Taro from '@tarojs/taro';
+import { Component } from 'react';
+import { View, Map, CoverView, CoverImage } from '@tarojs/components';
+import { AtTabBar } from 'taro-ui';
 import 'taro-ui/dist/style/components/tab-bar.scss';
 import 'taro-ui/dist/style/components/icon.scss';
-import './index.scss'
+import './index.scss';
 
 export default class Index extends Component {
   config = {
@@ -17,6 +17,7 @@ export default class Index extends Component {
     speed: null,
     accuracy: null,
     currentTab: 0,
+    markers: [],
   }
 
   componentDidMount () {
@@ -28,7 +29,24 @@ export default class Index extends Component {
           longitude: res.longitude,
           speed: res.speed,
           accuracy: res.accuracy,
-        })
+          markers: [{
+            id: 1,
+            latitude: res.latitude,
+            longitude: res.longitude,
+            callout: {
+              content: '面包树朴门永续',
+              color: '#ff0000',
+              fontSize: 14,
+              borderWidth: 2,
+              borderRadius: 10,
+              borderColor: '#000000',
+              bgColor: '#fff',
+              padding: 5,
+              display: 'ALWAYS',
+              textAlign: 'center',
+            }
+          }],
+        });
       }
     });
   }
@@ -42,7 +60,24 @@ export default class Index extends Component {
   render () {
     return (
       <View className='index'>
-        <Map longitude={this.state.longitude} latitude={this.state.latitude}/>
+        <Map
+          longitude={this.state.longitude}
+          latitude={this.state.latitude}
+          markers={this.state.markers}
+        >
+        {this.state.markers.map((marker) => (
+          <CoverView
+            className='marker'
+            style={{ width: '30px', height: '30px', lineHeight: '30px' }}
+            key={marker.id}
+            longitude={marker.longitude}
+            latitude={marker.latitude}
+          >
+            <CoverImage src={marker.iconPath} /> 
+            <CoverView>{marker.name}</CoverView>
+          </CoverView>
+        ))}
+        </Map>
         <AtTabBar
           fixed
           current={this.state.currentTab}
@@ -54,6 +89,6 @@ export default class Index extends Component {
           ]}
         />
       </View>
-    )
+    );
   }
 }
