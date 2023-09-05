6ac602d18d53339534a7b2ddcc223889cc37ad5c
Seaborn Lee <seabornlee.cn@gmail.com>
2023-09-05 16:21:50 +0800

    Submit new group request

--- src/pages/newGroup/index.tsx
+++ src/pages/newGroup/index.tsx
@@ -1,74 +1,75 @@
-import Taro from '@tarojs/taro'
-import { Component } from 'react'
-import { View, Text, Map } from '@tarojs/components'
-import { AtForm, AtInput, AtButton, AtImagePicker, AtTag } from 'taro-ui'
-import 'taro-ui/dist/style/components/form.scss'
-import 'taro-ui/dist/style/components/input.scss'
-import 'taro-ui/dist/style/components/button.scss'
-import 'taro-ui/dist/style/components/image-picker.scss'
-import 'taro-ui/dist/style/components/icon.scss'
-import './index.scss'
+import Taro from "@tarojs/taro";
+import { Component } from "react";
+import { View, Text, Map } from "@tarojs/components";
+import { AtForm, AtInput, AtButton, AtImagePicker, AtTag, AtMessage } from "taro-ui";
+import "taro-ui/dist/style/components/form.scss";
+import "taro-ui/dist/style/components/input.scss";
+import "taro-ui/dist/style/components/button.scss";
+import "taro-ui/dist/style/components/image-picker.scss";
+import "taro-ui/dist/style/components/icon.scss";
+import "./index.scss";
 
 export default class CreateGroup extends Component {
   state = {
     latitude: null,
     longitude: null,
-    name: '',
-    currentTagInput: '',
+    name: "",
+    currentTagInput: "",
     tags: [],
     images: [],
     location: null,
-    inputValueKey: 0
-  }
+    inputValueKey: 0,
+  };
 
   handleChangeName = (value) => {
     this.setState({
-      name: value
-    })
-  }
+      name: value,
+    });
+  };
 
   handleConfirm = (value, event) => {
-    if (value) { // Only add the tag if there's any
-      this.setState(prevState => ({
+    if (value) {
+      // Only add the tag if there's any
+      this.setState((prevState) => ({
         tags: [...prevState.tags, value],
-        currentTagInput: '',
-        inputValueKey: Math.random()
+        currentTagInput: "",
+        inputValueKey: Math.random(),
       }));
     }
-  }
+  };
 
   handleChangeImage = (files) => {
     this.setState({
-      images: files
-    })
-  }
+      images: files,
+    });
+  };
 
   handleChangeTag = (value, event) => {
     this.setState({
-      currentTagInput: value
-    })
-  }
+      currentTagInput: value,
+    });
+  };
 
   handleChooseLocation = async () => {
     try {
-      const res = await Taro.chooseLocation()
-      this.setState({ location: res })
+      const res = await Taro.chooseLocation();
+      this.setState({ location: res });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
-  componentDidMount () {
+  componentDidMount() {
     Taro.getLocation({
-      type: 'wgs84',
+      type: "wgs84",
       success: (res) => {
         this.setState({
           latitude: res.latitude,
           longitude: res.longitude,
           speed: res.speed,
-          accuracy: res.accuracy
+          accuracy: res.accuracy,
         });
-      }
+      },
     });
   }
 
@@ -78,26 +79,65 @@ export default class CreateGroup extends Component {
     }));
   };
 
-  handleSubmit = () => {
-    if(!this.state.name || !this.state.tags || this.state.images.length === 0) {
-      Taro.atMessage({ 'message': 'All fields are required!', 'type': 'error', })
-    } else {
-      // submit form
+  handleSubmit = async () => {
+    if (
+      !this.state.name ||
+      !this.state.tags ||
+      this.state.images.length === 0
+    ) {
+      Taro.atMessage({ message: "必填字段不能为空！", type: "error" });
+      return;
     }
-  }
+
+    const { name, tags, images, location } = this.state;
+
+    try {
+      const response = await Taro.request({
+        url: "http://localhost:1337/group", // replace with your POST url
+        method: "POST",
+        data: {
+          name,
+          tags: JSON.stringify(tags),
+          images: JSON.stringify(images),
+          location: JSON.stringify(location),
+          me: 'seaborn'
+        },
+        header: {
+          "content-type": "application/json",
+        },
+      });
+
+      if (response.statusCode !== 200) {
+        throw new Error(`HTTP Error: ${response.statusCode}`);
+      }
+
+      console.log(response.data);
+
+      // handle incoming data, whether it's error or success
+      if (response.data.error) {
+        Taro.atMessage({ message: response.data.error, type: "error" });
+      } else {
+        Taro.atMessage({
+          message: "Group successfully created!",
+          type: "success",
+        });
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   render() {
     return (
-      <View className='page'>
-        <AtForm
-          onSubmit={this.handleSubmit}
-        >
-          <View className='row'>
+      <View className="page">
+        <AtMessage />
+        <AtForm onSubmit={this.handleSubmit}>
+          <View className="row">
             <Text>名称</Text>
             <AtInput
-              name='groupName'
-              type='text'
-              placeholder='Enter group name'
+              name="groupName"
+              type="text"
+              placeholder="Enter group name"
               value={this.state.name}
               onChange={this.handleChangeName}
             />
@@ -106,29 +146,29 @@ export default class CreateGroup extends Component {
             <Text>成员标签</Text>
             <AtInput
               key={this.state.inputValueKey}
-              name='tags'
-              type='text'
+              name="tags"
+              type="text"
               focus
-              placeholder='Enter tag'
+              placeholder="Enter tag"
               value={this.state.currentTagInput}
               onConfirm={this.handleConfirm}
               onChange={this.handleChangeTag}
             />
-            <View className='tags'>
+            <View className="tags">
               {this.state.tags.map((tag, index) => (
-                <View className='tag-container'>
-                  <AtTag key={index} type='primary' circle>{tag}</AtTag>
-                  <Text 
-                    className="at-icon at-icon-close-circle" 
+                <View className="tag-container">
+                  <AtTag key={index} type="primary" circle>
+                    {tag}
+                  </AtTag>
+                  <Text
+                    className="at-icon at-icon-close-circle"
                     onClick={() => this.handleDeleteTag(index)}
-                    style={
-                      {
-                        position: 'relative',
-                        left: '-18px',
-                        top: '-10px',
-                        color: '#ff0000'
-                      }
-                    }
+                    style={{
+                      position: "relative",
+                      left: "-18px",
+                      top: "-10px",
+                      color: "#ff0000",
+                    }}
                   />
                 </View>
               ))}
@@ -147,13 +187,19 @@ export default class CreateGroup extends Component {
             <Map
               longitude={this.state.longitude}
               latitude={this.state.latitude}
-              onClick={this.handleChooseLocation}>
-            </Map>
+              onClick={this.handleChooseLocation}
+            ></Map>
           </View>
         </AtForm>
-        <AtButton type='primary' className="submit-button" formType='submit'>创建</AtButton>
+        <AtButton
+          type="primary"
+          className="submit-button"
+          formType="submit"
+          onClick={this.handleSubmit}
+        >
+          创建
+        </AtButton>
       </View>
-    )
+    );
   }
 }
-
