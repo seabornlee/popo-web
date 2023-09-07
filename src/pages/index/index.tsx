e2f078268922cd77891a8fecb738cf9a4ec90af3
Seaborn Lee <seabornlee.cn@gmail.com>
2023-09-07 12:29:30 +0800

    View selected group

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,14 @@
 import Taro from "@tarojs/taro";
 import { Component } from "react";
-import { View, Map, CoverView, CoverImage } from "@tarojs/components";
-import { AtTabBar, AtActionSheet, AtActionSheetItem } from "taro-ui";
+import { View, Map, CoverView, Image } from "@tarojs/components";
+import {
+  AtTabBar,
+  AtActionSheet,
+  AtActionSheetItem,
+  AtFloatLayout,
+  AtCard,
+  AtTag,
+} from "taro-ui";
 import "taro-ui/dist/style/components/tab-bar.scss";
 import "taro-ui/dist/style/components/icon.scss";
 import "./index.scss";
@@ -15,46 +22,53 @@ export default class Index extends Component {
     currentTab: 0,
     markers: [],
     showLogin: false,
-    token: null
+    token: null,
+    groups: [],
+    selectedGroup: null,
   };
 
   async componentDidMount() {
     this.setState({
-      token: await Taro.getStorageSync("token")
+      token: await Taro.getStorageSync("token"),
     });
 
     const response = await Taro.request({
       url: "http://localhost:1337/group",
       method: "GET",
       header: {
-        "content-type": "application/json"
+        "content-type": "application/json",
       },
     });
-    const groups = response.data;
-    const markers = groups
-      .map((group) => ({
-        id: group.id,
-        name: group.name,
-        tags: JSON.parse(group.tags),
-        images: JSON.parse(group.images),
-        location: JSON.parse(group.location),
-      }))
-      .map((group) => ({
-        id: group.id,
-        latitude: group.location.latitude,
-        longitude: group.location.longitude,
-        callout: {
-          content: group.name,
-          color: "#000000",
-          fontSize: 14,
-          borderWidth: 1,
-          borderRadius: 10,
-          borderColor: "#000000",
-          padding: 5,
-          display: "ALWAYS",
-          textAlign: "center",
-        },
-      }));
+    const groups = response.data.map((group) => ({
+      id: group.id,
+      name: group.name,
+      tags: JSON.parse(group.tags),
+      images: JSON.parse(group.images),
+      location: JSON.parse(group.location),
+    }));
+
+    console.log(response.data);
+
+    this.setState({
+      groups: groups,
+    });
+
+    const markers = groups.map((group) => ({
+      id: group.id,
+      latitude: group.location.latitude,
+      longitude: group.location.longitude,
+      callout: {
+        content: group.name,
+        color: "#000000",
+        fontSize: 14,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: "#000000",
+        padding: 5,
+        display: "ALWAYS",
+        textAlign: "center",
+      },
+    }));
     console.log(markers);
     Taro.getLocation({
       type: "wgs84",
@@ -81,11 +95,20 @@ export default class Index extends Component {
       // if (this.state.token) {
       //   Taro.navigateTo({ url: "/pages/newGroup/index" });
       // } else {
-        // this.setState({
-        //   showLogin: true,
-        // });
+      // this.setState({
+      //   showLogin: true,
+      // });
       // }
     }
+  };
+
+  handleMarkerTap = (e) => {
+    console.log("marker tap", e);
+    const groupId = e.markerId;
+    const group = this.state.groups.find((g) => g.id === groupId);
+    this.setState({
+      selectedGroup: group,
+    });
   };
 
   login = () => {
@@ -127,6 +150,13 @@ export default class Index extends Component {
     return isValidFormat;
   };
 
+  handleMapClick = (e) => {
+    console.log("map click", e);
+    this.setState({
+      selectedGroup: null
+    })
+  }
+
   render() {
     return (
       <View className='index'>
@@ -134,17 +164,42 @@ export default class Index extends Component {
           longitude={this.state.longitude}
           latitude={this.state.latitude}
           markers={this.state.markers}
+          onmarkertap={this.handleMarkerTap}
         >
           {this.state.markers.map((marker) => (
             <CoverView
               className='marker'
-              style={{ width: "30px", height: "30px", lineHeight: "30px" }}
               key={marker.id}
               longitude={marker.longitude}
               latitude={marker.latitude}
             ></CoverView>
           ))}
         </Map>
+        {this.state.selectedGroup && (
+          <View className='group-container'>
+            <View className='at-row'>
+              <View className='at-col'>
+                <Image src='https://cdn.pixabay.com/photo/2023/08/26/17/49/dahlias-8215514_1280.jpg' />
+              </View>
+              <View className='at-col'>
+                <View className='group-name'>{this.state.selectedGroup.name}</View>
+                <View className='tags'>
+                  {this.state.selectedGroup.tags.map((tag, index) => (
+                    <View className='tag-container'>
+                      <AtTag key={index} type='primary' circle>
+                        {tag}
+                      </AtTag>
+                    </View>
+                  ))}
+                </View>
+                <View className='member-count'>38</View> 位成员，
+                <View className='member-count'>12</View> 场活动
+                <View>📍 &nbsp;{this.state.selectedGroup.location.name}</View>
+                <View>🧭 &nbsp;距您直线2.3公里</View>
+              </View>
+            </View>
+          </View>
+        )}
         <AtTabBar
           fixed
           current={this.state.currentTab}
@@ -157,7 +212,7 @@ export default class Index extends Component {
         />
         <AtActionSheet isOpened={this.state.showLogin} cancelText='取消'>
           <AtActionSheetItem onClick={this.login}>微信登录</AtActionSheetItem>
-        </AtActionSheet>{" "}
+        </AtActionSheet>
       </View>
     );
   }
